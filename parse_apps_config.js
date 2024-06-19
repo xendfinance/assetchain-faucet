cafb31725b452746fc3705b074c478537fee5768
ogbuifymark <ogbuifymark@gmail.com>
2024-06-19 20:44:02 +0100

    added pm2 files

---
+++ parse_apps_config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'faucet',           // Application name
+        script: 'dist/app.js', // Script to be run
+        instances: 1,          // Number of instances to be started
+        autorestart: true,     // Automatically restart crashed application
+        watch: false,          // Watch files for changes
+        max_memory_restart: '1G', // Restart the application if it reaches this memory usage
+      }
+    ]
+  };

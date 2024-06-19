2f912c024ce41636d24c31a49d310d98ce62393f
MacBook Pro <macbookpro@ip-192-168-234-118.eu-west-1.compute.internal>
2024-06-19 20:50:36 +0100

    added pm2 files

--- parse_apps_client.config.cjs
+++ parse_apps_client.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'faucet-client',           // Application name
-        script: './build-client.js', // Script to be run
+        script: './faucet-client/build-client.js', // Script to be run
         instances: 1,          // Number of instances to be started
         autorestart: true,     // Automatically restart crashed application
         watch: false,          // Watch files for changes
